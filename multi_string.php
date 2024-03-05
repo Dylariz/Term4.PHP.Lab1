dfac66e6d33b85dced2a642ab1060098498b28c6
Dylariz <zarec-k@mail.ru>
2024-03-05 15:05:27 +0300

    Optimize multi_string.php

--- multi_string.php
+++ multi_string.php
@@ -1,18 +1,21 @@
 <?php
-
-for ($t = 0; $t < 100; $t++) {
-    $count = 0;
-    for ($i = 10; $i <= 23; $i++) {
-        if ($i >= 12 && $i <= 15) {
-            echo $i . " " . $i . " ";
-            $count += 2;
-        } elseif ($i != 16) {
-            echo $i . " ";
-            $count++;
-        }
-        if ($count % 17 == 0) {
-            echo "<br>";
-        }
+$str = "";
+$count = 0;
+for ($i = 10; $i <= 23; $i++) {
+    if ($i >= 12 && $i <= 15) {
+        $str .= $i . " " . $i . " ";
+        $count += 2;
+    } elseif ($i != 16) {
+        $str .= $i . " ";
+        $count++;
     }
+    if ($count % 17 == 0) {
+        $str .= "<br>";
+    }
+}
+
+// Вывод
+for ($t = 0; $t < 100; $t++) {
+    echo $str;
 }
 ?>
